84323719617f6894a0cab289227aa9de3715be44
ovlb <o@ovlb.net>
2014-08-30 15:32:17 +0200

    js done

--- js/quiz.js
+++ js/quiz.js
@@ -4,70 +4,45 @@ var results = ['meta', 'rotis', 'garamond', 'minion', 'palatino', 'avenir', 'uni
 // Variable, um zu gucken, ob es Fehler gibt, in der check-Funktion benoetigt
 var errors = 0;
 
+// Variablen fuer die Fehler-/Erfolgsmeldung
+var msg;
+var newclass;
+
+// Die Richtigkeit der Antworten ueberpruefen, mit (index) kann beim Aufrufen der Funktion eine Variable uebergeben werden
 function checkAnswer(index) {
 	// In dieser Variable wird der Wert des jeweiligen select-Feldes gespeichert; index+1, da die erste ID im HTML 1 ist, der erste Wert in Javascript aber 0
 	var answer = document.getElementById('schriftwahl-' + (index+1)).value;
-	console.log('answer ==' + answer);
 	
-	// Wenn der Wert des select-Felds von dem im results-Arrys gespeicherten abweicht, werden die Fehler hochgezaehlt und das beinhaltende div mit einer Fehlerklasse versehen
+	// Wenn der Wert des select-Felds von dem im results-Arrys gespeicherten abweicht, werden die Fehler hochgezaehlt und das beinhaltende div mit der Fehlerklasse versehen
+	// Wenn die Antwort richtig ist, wird das div mit der Erfolgsklasse eingefaerbt
 	if (answer != results[index]) {
 		errors++;
-		console.log('errors ==' + errors);
-		document.getElementById('buchstabe-' + (index+1)).className += ' error';
+		document.getElementById('buchstabe-' + (index+1)).setAttribute('class', 'buchstabe col third error');
+	} else {
+		document.getElementById('buchstabe-' + (index+1)).setAttribute('class', 'buchstabe col third success');
 	}
 }
 
-// Fehlermeldung anzeigen; diese automatisch wieder ausblenden
-function hasErrors() {
-	var $msg = '<div class="error">Ooops.' + errors + 'Angaben waren nicht korrekt.</div>';
-	
-	$msg.css({
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            //background: '#e31217',
-			width: '100%',
-			'z-index': 9999999999,
-			display: 'block',
-			'text-indent': '32px',
-			padding: '5px 0'
-		})
-		.hide()
-		.appendTo('#result')
-		.show();
-	setTimeout(function() {
-		$msg.fadeOut(800, function() { $msg.remove(); });
-	}, 3000);
-}
-
-function hasNoErrors() {
-	var $msg = '<div class="success">Herzlichen Gl&uuml;ckwunsch! Du hast alles richtig beantwortet.</div>';
-	
-	$msg.css({
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            //background: '#e31217',
-			width: '100%',
-			'z-index': 9999999999,
-			display: 'block',
-			'text-indent': '32px',
-			padding: '5px 0'
-		})
-		.hide()
-		.before('#container')
-		.show();
+// Erfolgs-/Fehlermeldung definieren, feedback-div einfaerben
+function giveFeedback(theMessage, theClass) {
+	document.getElementById('feedback').innerHTML = theMessage;
+	document.getElementById('feedback').setAttribute('class', theClass);
 }
 
 function check() {
-	// Rufe die Funktion checkAnswer fuer jedes Element des Loesungs-Arrays auf
+	// Fehler (wieder) auf 0 setzen, um bei zweitem Versuch eine korrekte Anzahl Fehler zu zeigen
+	errors = 0;
+	// Rufe die Funktion checkAnswer fuer jedes Element des Loesungs-Arrays auf und uebergebe die Stelle im Array
 	for (i = 0; i < results.length; i++) {
 		checkAnswer(i);
 	}
 	
 	if (errors > 0) {
-		hasErrors();
+		msg = '<p>Ooops. ' + errors + ' Angaben waren nicht korrekt. Versuche es noch mal.</p>';
+		newclass = 'error';
 	} else {
-		hasNoErrors();
+		msg = '<p>Herzlichen Gl&uuml;ckwunsch! Du hast alles richtig beantwortet.</p>';
+		newclass = 'success';
 	}
+	giveFeedback(msg, newclass);
 }
