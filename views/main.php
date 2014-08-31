98618a92934576e4338b943818b071bde5d852ff
ovlb <o@ovlb.net>
2014-08-31 22:44:53 +0200

    basic markup structure main.php

--- views/main.php
+++ views/main.php
@@ -1,3 +1,26 @@
 <main id="quiz" class="row clearfix">
-	
+	<section id="intro">
+		<h3>Hi!</h3>
+		<p>Lorem ipsum</p>
+	</section>
+	<section id="options">
+		<div class="row clearfix">
+			<div class="col third" id="input-number">
+				<h4>Number</h4>
+				<input type="range" name="input-number" min="3" max="12" step="3">
+			</div><!-- End #input-number -->
+			<div class="col third" id="input-letter">
+				<h4>Which</h4>
+				<input id="letter-e" type="radio" name="letter" value="e"><label for="letter-e">e</label>
+				<input id="letter-amp" type="radio" name="letter" value="amp"><label for="letter-amp">&amp;</label>
+				<input id="letter-a" type="radio" name="letter" value="a"><label for="letter-a">a</label>
+			</div><!-- End #input-letter -->
+			<div class="col third" id="input-type">
+				<h4>What?</h4>
+				<input id="fonts-both" type="radio" name="fonts-both" value="both"><label for="fonts-both">Both</label>
+				<input id="fonts-serif" type="radio" name="fonts-serif" value="serif"><label for="fonts-serif">Serif</label>
+				<input id="fonts-sans" type="radio" name="fonts-sans" value="sans"><label for="fonts-sans">Sans-serif</label>
+			</div><!-- Ende #input-type -->
+		</div><!-- End row selects -->
+	</section>
 </main>
