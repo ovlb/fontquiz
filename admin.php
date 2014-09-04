581287be02d503e9b647b490d2072ecc4d53b2cd
ovlb <o@ovlb.net>
2014-09-04 10:07:23 +0200

    admin done

--- admin.php
+++ admin.php
@@ -37,7 +37,7 @@ function login() {
 	
 	// Try to log the user in
 	// Or just display the login form
-	if (isset($_POST['login'])) {
+	if ( isset($_POST['login']) ) {
 		// Check credentials 
 		// Create a session and display the admin menu if login attempt succeeds 
 		// Otherwise ask the user to try again
@@ -53,7 +53,7 @@ function login() {
 	}
 }
 
-// On logout, kill the session and load admin.php
+// On logout, unset the session and load admin.php
 function logout() {
 	unset $_SESSION['username'];
 	header('Location: admin.php)';
@@ -64,7 +64,70 @@ function newFont() {
 	$results['pageTitle'] = 'Upload new font';
 	$results['formAction'] = 'newFont';
 	
-	if (isset($_POST['saveChanges'])) {
-		
+	if ( isset($_POST['saveChanges']) ) { // The user wants to save changes
+		$article = new Font;
+		$article->storeFormValues($_POST);
+		$article->insert();
+		header('Location: admin.php?status=changesSaved');
+	} elseif ( isset($_POST['cancel']) ) { // The user wants to cancel and delete the input
+		header('Location: admin.php');
+	} else {
+		$results['article'] = new Font;
+		require(TEMPLATE_PATH . '/admin/edit.php');
 	}
 }
+
+
+funtion editFont() {
+	$results = array();
+	$results['pageTitle'] = 'Edit Font';
+	$results['formAction'] = 'editFont';
+	
+	if (isset($_POST['saveChanges'])) { // The user wants to save changes
+		if (!$article = Font::getById( (int)$_POST['fontId'] )) { // Return an error message if the ID does not exist
+			header('Location: admin.php?error=fontNotFound')
+			return;
+		}
+		
+		// Save changes if everything is fine
+		$article->storeFormValues($_POST);
+		$article->update();
+		header('Location: admin.php?status=changesSaved');
+		
+	} elseif ( isset($_POST['cancel']) ) {
+		header('location: admin.php');
+	} else {
+		$results['article'] = Font::getById( int()$_GET['fontId'] );
+		require(TEMPLATE_PATH . '/admin/edit.php');
+	}
+}
+
+funtion deleteFont() {
+	if (!$article = Font::getById( (int)$_POST['fontId'] )) { // Return an error message if the ID does not exist
+			header('Location: admin.php?error=fontNotFound')
+			return;
+	}
+	
+	$article->delete();
+	header('Location: admin.php?status=fontDeleted');
+}
+
+function listFonts() {
+	$results = array();
+	$data = Font::getList();
+	$results['articles'] = $data['results'];
+	$results['totalRows'] = $data['totalRows'];
+	$results['pageTitle'] = 'All Fonts'; 
+	
+	if ( isset( $_GET['error'] ) ) {
+		if ( $_GET['error'] == "articleNotFound" ) $results['errorMessage'] = "Error: Article not found.";
+	}
+ 
+	if ( isset( $_GET['status'] ) ) {
+		if ( $_GET['status'] == "changesSaved" ) $results['statusMessage'] = "Your changes have been saved.";
+		if ( $_GET['status'] == "articleDeleted" ) $results['statusMessage'] = "Article deleted.";
+	}
+ 
+	require( TEMPLATE_PATH . "/admin/listArticles.php" );
+}
+?>
